1bdcf28034a0bacc20c05aca1c17a3d74d4bd523
Imtiyaz Moon <imtiyaz.moon@yahoo.com>
2021-04-29 14:43:10 +0600

    dynamic routes implemented

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,45 @@
-console.log("Hello Moon");
+import express, { Application,Request,Response } from "express";
+
+export type Handler = (req:Request,res:Response) => void
+
+interface IRoute{
+    http: string,
+    path: string,
+    handler: Handler
+}
+
+const init = (): Application => {
+    let app = express();
+
+    let routes:IRoute[] = [
+        {
+            http: "get",
+            path: "/",
+            handler: (req:Request,res:Response)=>{
+                res.send(`requesting again\n ${req.method} at \n`+ new Date())
+            }
+        },
+        {
+            http: "get",
+            path: "/dictionary",
+            handler: (req:Request,res:Response)=>{
+                res.send(`Dictinary 2\n ${req.method} at \n`+ new Date() );
+            }
+        }
+    ]
+
+    // app.get('/', (req,res)=>{
+    //     res.send(`requesting \n ${req.method} at \n`+ new Date())
+    // })
+
+    routes.forEach((route)=>{
+        (app as any)[route.http](route.path,route.handler)
+    })
+
+    return app;
+}
+
+const app = init();
+app.listen(3000,()=>{
+    console.log("server is listening");
+})
